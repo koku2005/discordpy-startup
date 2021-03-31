abbb73ed4ee3bc80d65b033e8cf89e315f5bbeed
koku2005 <81688298+koku2005@users.noreply.github.com>
2021-03-31 21:27:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,8 +14,8 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def sex(ctx):
+    await ctx.send('oooooooooo')
 
 
 bot.run(token)
